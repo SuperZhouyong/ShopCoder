d12e3c61042c0bdf0311e44a35c70d8fa5e76afb
SuperZhouyong <z_zhouyong@163.com>
2018-05-14 19:45:44 +0800

    [20180514]

---
+++ app/src/main/java/com/intention/sqtwin/ui/myinfo/contract/LoginContract.java
@@ -0,0 +1,11 @@
+package com.intention.sqtwin.ui.myinfo.contract;
+
+/**
+ * Description: 绝无Bug
+ * Data：2018/5/14 0014-下午 13:32
+ * Blog：Super简单
+ * Author: ZhouYong
+ */
+
+public class LoginContract {
+}
---
+++ app/src/main/java/com/intention/sqtwin/ui/myinfo/presenter/LoginPresenter.java
@@ -0,0 +1,11 @@
+package com.intention.sqtwin.ui.myinfo.presenter;
+
+/**
+ * Description: 绝无Bug
+ * Data：2018/5/14 0014-下午 13:35
+ * Blog：Super简单
+ * Author: ZhouYong
+ */
+
+class LoginPresenter {
+}
