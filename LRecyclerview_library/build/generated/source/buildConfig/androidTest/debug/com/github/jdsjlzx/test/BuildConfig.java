c57c9a2204953c39375ffb031f8876c4d5b34541
SuperZhouyong <z_zhouyong@163.com>
2018-05-16 01:57:44 +0800

    [2018516晚上]

---
+++ LRecyclerview_library/build/generated/source/buildConfig/androidTest/debug/com/github/jdsjlzx/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.github.jdsjlzx.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.github.jdsjlzx.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1400;
+  public static final String VERSION_NAME = "2.4.0-debug";
+}
